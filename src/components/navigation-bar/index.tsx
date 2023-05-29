ad606f2391741c9cabcbbf5857e18215449cb5d7
danusorn23456 <danusorn23456@gmail.com>
2023-05-29 09:43:27 +0700

    change projectname

--- src/components/navigation-bar/index.tsx
+++ src/components/navigation-bar/index.tsx
@@ -13,7 +13,7 @@ function NavigationBar({ ...rest }: NavigationBarProps) {
         <div className='fixed h-14 w-full top-0 left-0 right-0 bg-gradient-to-b from-gray-950 backdrop-blur-sm z-10' {...rest}>
             <div className='container max-w-6xl mx-auto h-full flex items-center px-4'>
                 <img src={logoSvg} className="w-8 mr-2" />
-                <h2 className='uppercase text-white text-2xl font-bold'>kit creative</h2>
+                <h2 className='uppercase text-white text-lg font-bold'>kit like creative</h2>
             </div>
         </div>
     )
