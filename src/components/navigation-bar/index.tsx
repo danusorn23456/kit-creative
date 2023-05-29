ef681cbfdda806132c7b73f90042274e2650c0e0
danusorn23456 <danusorn23456@gmail.com>
2023-05-29 09:31:18 +0700

    config logo

--- src/components/navigation-bar/index.tsx
+++ src/components/navigation-bar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import logoSvg from "assets/kc.svg"
 
 export interface NavigationBarProps {
 
@@ -12,6 +12,7 @@ function NavigationBar({ ...rest }: NavigationBarProps) {
     return (
         <div className='fixed h-14 w-full top-0 left-0 right-0 bg-gradient-to-b from-gray-950 backdrop-blur-sm z-10' {...rest}>
             <div className='container max-w-6xl mx-auto h-full flex items-center px-4'>
+                <img src={logoSvg} className="w-8 mr-2" />
                 <h2 className='uppercase text-white text-2xl font-bold'>kit creative</h2>
             </div>
         </div>
