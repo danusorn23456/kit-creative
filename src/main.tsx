81894e6c8e7219da0c0b3ad22e9a14274a98c1af
danusorn23456 <danusorn23456@gmail.com>
2023-06-04 23:28:37 +0700

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Routes } from './router/index.tsx'
-import "styles/index.css"
+import ReactDOM from "react-dom/client";
+import { Routes } from "./router/index.tsx";
+import "styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Routes />
+);
